4b66d3047fd3ab9453664496d2790460a3934b30
sahosridoy <sahosmia.webdev@gmail.com>
2022-12-10 19:59:23 +0600

    Create Object and Serch input is working

---
+++ js/friends_list.js
@@ -0,0 +1,52 @@
+let friends_list =[
+    {
+        'id' : "1",
+        'name' : "Sahos Mia",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+    {
+        'id' : "2",
+        'name' : "Momin",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+    {
+        'id' : "3",
+        'name' : "Asif",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+    {
+        'id' : "4",
+        'name' : "Sobuj",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+    {
+        'id' : "5",
+        'name' : "Robin",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+    {
+        'id' : "6",
+        'name' : "Sujon",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+    {
+        'id' : "7",
+        'name' : "Sahos Ridoy",
+        'phone' : "01952827301",
+        'date_of_birth' : "14 Dec, 2022",
+        'blood' : "O-",
+    },
+
+];
