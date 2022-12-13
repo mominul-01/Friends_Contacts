659ac50456567371bae65d6f4533d265ccff0688
sahosridoy <sahosmia.webdev@gmail.com>
2022-12-14 02:48:50 +0600

    data show done by modal

--- js/main.js
+++ js/main.js
@@ -1,45 +1,84 @@
 let input = document.querySelector('#search');
 let showResult = document.querySelector('#parent-list');
+let details = document.querySelector('.details_page_h1');
+let ulList = document.getElementById("parent-list");
+let person_details = document.querySelector(".person_details");
 
+
+
+if(input !== undefined && input !== null){
+    input.addEventListener('keypress', function() {
+        var text = '';
+        let searchArry = [];
         
-input.addEventListener('keypress', function() {
-    var text = '';
-    let searchArry = [];
 
-    if (input.value != "") {
+        if (input.value != "") {
+            var li = '';
 
-        for (const friend of friends_list) {
-            if (friend.name.match(input.value) != null) {
-                searchArry.push(friend.name);
+            for (const [i,friend] of friends_list.entries()) {
+                if (friend.name.match(input.value) != null || friend.blood.match(input.value) != null) {
+                    searchArry.push(i);
+                }
+            }
+
+            if(searchArry.length != 0){
+                for(let friends of searchArry){
+                    var liSingle = `<li><button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" value="${friends}" >${friends_list[friends].name} - ${friends_list[friends].phone}</button></li>`;
+                    li = li.concat(liSingle);
+                }
             }
         }
+
+        text = '<ul>' + li + '</ul>';
         
-        var li = '';
-    
-        for(let friends of searchArry){
-            var liSingle = `<li><a value="${friends}" >${friends}</a></li>`;
-                li = li.concat(liSingle);
+        if (searchArry.length != 0) {
+                showResult.innerHTML = text;
             
+        }else{
+            
+            showResult.innerHTML = `<span>Anything Wrong</span>`;
         }
-    }
-    
-    text = '<ul>' + li + '</ul>';
-    if (searchArry.length != 0) {
-            showResult.innerHTML = text;
-           
-    }else{
+    });
+}
+
+if(ulList !== undefined && ulList !== null){
+    var text2 = '';
+    var li2 = '';
+    ulList.addEventListener("click",function(e) {
+        var clickAbleID = "";
+        clickAbleID = e.target.getAttribute('value');
+        details.textContent = friends_list[clickAbleID].name;
         
-        showResult.innerHTML = `<span>Anything Wrong</span>`;
-    }
-});
+        for(const item in friends_list[clickAbleID]) {
+            if( item !== 'name' && friends_list[clickAbleID][item] !== null){
+                var liSingle2 = `<li>${item} : ${friends_list[clickAbleID][item]}</li>`;
+                li2 = li2.concat(liSingle2);
+            }
+        }
+
+        text2 = '<ul>' + li2 + '</ul>';
+        person_details.innerHTML = text2;
+
+    });
+}
 
 
-let ulList = document.getElementById("parent-list");
-ulList.addEventListener("click",function(e) {
-    console.log(e.target.getAttribute('value'));
-     alert(e.target.getAttribute('value'));
-});
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
